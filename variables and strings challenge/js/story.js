872870bb5d82dd9653fb6b3af4e42a10a69982f5
Jeph Mari Daligdig <daligdigjeph09@gmail.com>
2024-06-28 15:28:35 +0800

    variables and strings challenge

---
+++ variables and strings challenge/js/story.js
@@ -0,0 +1,12 @@
+// 1. Declare variables and capture input.
+let adjective = prompt('Please type an adjective.');
+let verb = prompt('Please type a verb.');
+let noun = prompt('Please type a noun.');
+
+// 2. Combine the input with other words to create a story.
+const story = "Once upon a time, in a " + adjective + " land, there was a " + noun + " who loved to " + verb +
+    " all day long. People from all around came to see the amazing " + noun + " and watch it " +
+    verb + ". It was truly a " + adjective + " sight to behold.";
+
+// 3. Display the story as a <p> inside the <main> element.
+document.querySelector('main').innerHTML = story;
